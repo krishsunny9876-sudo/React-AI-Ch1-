50c4752a41da6c0f8f499e4de6b4a7d5a41bed8d
Kirtan <krishsunny9876@gmail.com>
2026-03-13 21:54:21 +0530

    creating navbar #1

---
+++ src/Componenets/home.jsx
@@ -0,0 +1,13 @@
+import './CSS/style.css'
+
+export default function My_Page() {
+
+
+    return (
+        <>
+            <h1>
+                Mini Task Tracker
+            </h1>
+        </>
+    )
+}
---
+++ src/Componenets/navbar.jsx
@@ -0,0 +1,18 @@
+import './CSS/style.css'
+
+export default function My_Page() {
+
+
+    return (
+        <>
+            <nav className="navbar">
+                <h2>Welcome</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>Tasks</li>
+                    <li>Stats</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
