15dc45402d6b3bd2b77cd917f83b94a47b21e9e2
Kirtan <krishsunny9876@gmail.com>
2026-03-13 23:21:03 +0530

    Router Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 import Navbar from './Componenets/navbar'
 import HomePage from './Componenets/home'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (<>
+  return (<Router>
+    <Routes>
+      <Route
+        path="/header"
+        element={<MyPage alert={setting_alert} />}
+      />
+    </Routes>
     <Navbar />
     <HomePage />
-  </>)
+  </Router>)
 }
 
 export default App
---
+++ src/Componenets/taskbar.jsx
@@ -0,0 +1,19 @@
+import './CSS/style.css'
+import './CSS/container.css'
+
+export default function My_Page() {
+
+
+    return (
+        <>
+            <div className="area">
+                <h2>Task Manager</h2>
+
+                <div className="block">
+                    <input type="text" placeholder='Task Name' />
+                    <button className='add_btn'>Add</button>
+                </div>
+            </div>
+        </>
+    )
+}
